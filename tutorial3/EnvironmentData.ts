15a602c36e4e76012eb2407a006440eeff87c95f
michaelprosario <michael.p.rosario@gmail.com>
2024-06-29 12:45:43 +0000

    feat ( tutorial 3 ) pgvector rag - it works!

---
+++ tutorial3/EnvironmentData.ts
@@ -0,0 +1,8 @@
+
+export class EnvironmentData {
+  DbHost: string | undefined;
+  DbName: string | undefined;
+  DbPassword: string | undefined;
+  DbPort: number = 0;
+  UserName: string | undefined;
+}
--- tutorial3/index.ts
+++ tutorial3/index.ts
@@ -6,61 +6,106 @@ import { createStuffDocumentsChain } from "langchain/chains/combine_documents";
 import { ChatPromptTemplate } from "@langchain/core/prompts";
 import { CheerioWebBaseLoader } from "@langchain/community/document_loaders/web/cheerio"
 import { createRetrievalChain } from "langchain/chains/retrieval";
+
+import {
+  DistanceStrategy,
+  PGVectorStore,
+  PGVectorStoreArgs,
+} from "@langchain/community/vectorstores/pgvector";
+import { PoolConfig } from "pg";
+import { EnvironmentData } from "./EnvironmentData";
+
 const readlineSync = require('readline-sync');
 
+function getEnvironmentVariables() {
+  let response = new EnvironmentData();
+  response.DbHost = process.env.DbHost;
+  response.UserName = process.env.UserName;
+  response.DbPassword = process.env.DbPassword;
+  response.DbName = process.env.DbName;
+  response.DbPort = parseInt(process.env.DbPort + "");
+
+  return response;
+}
+
+let envData = getEnvironmentVariables();
+console.log(envData);
+
+const config: PGVectorStoreArgs = {
+  postgresConnectionOptions: {
+    type: "postgres",
+    host: envData.DbHost,
+    port: envData.DbPort,
+    user: envData.UserName,
+    password: envData.DbPassword,
+    database: envData.DbName,
+  } as PoolConfig,
+  tableName: "content1",
+  columns: {
+    idColumnName: "id",
+    vectorColumnName: "vector",
+    contentColumnName: "content",
+    metadataColumnName: "metadata",
+  },
+  // supported distance strategies: cosine (default), innerProduct, or euclidean
+  distanceStrategy: "cosine" as DistanceStrategy,
+};
+
+
+
 async function main() {
-  // const loader = new CheerioWebBaseLoader(
-  //   "https://www.vatican.va/content/john-paul-ii/en/letters/1994/documents/hf_jp-ii_let_02021994_families.html"
-  // );
+  console.log("start main...")
+ 
+  console.log("start create embeddings");
+  let embeddings = new OpenAIEmbeddings();
+  console.log("end create embeddings");
 
-  // const docs = await loader.load();
+  const pgvectorStore = await PGVectorStore.initialize(embeddings,config);
 
-  // const splitter = new RecursiveCharacterTextSplitter();
+  console.log("pgvectorStore started...")
 
-  // const splitDocs = await splitter.splitDocuments(docs);
+  await pgvectorStore.addDocuments([
+    { pageContent: "what's this", metadata: { a: 2 } },
+    { pageContent: "Cat drinks milk", metadata: { a: 1 } },
+  ]);
 
-  // const embeddings = new OpenAIEmbeddings();
+  console.log("documents added")
 
-  // const vectorstore = await MemoryVectorStore.fromDocuments(
-  //   splitDocs,
-  //   embeddings
-  // );
+  const splitter = new RecursiveCharacterTextSplitter();
 
+  const prompt =
+    ChatPromptTemplate.fromTemplate(`Answer the following question based only on the provided context:
+  <context>
+  {context}
+  </context>
 
-  // const prompt =
-  //   ChatPromptTemplate.fromTemplate(`Answer the following question based only on the provided context:
-  
-  // <context>
-  // {context}
-  // </context>
-  
-  // Question: {input}`);
+  Question: {input}`);
 
-  // const model = new ChatOpenAI({});
+  const model = new ChatOpenAI({});
 
-  // const documentChain = await createStuffDocumentsChain({
-  //   llm: model,
-  //   prompt,
-  // });
+  const documentChain = await createStuffDocumentsChain({
+    llm: model,
+    prompt,
+  });
 
-  // const retriever = vectorstore.asRetriever();
+  const retriever = pgvectorStore.asRetriever();
 
-  // const retrievalChain = await createRetrievalChain({
-  //   combineDocsChain: documentChain,
-  //   retriever,
-  // });
+  const retrievalChain = await createRetrievalChain({
+    combineDocsChain: documentChain,
+    retriever,
+  });
 
-  // while(true)
-  // {
-  //   let question = readlineSync.question(">");
+  while(true)
+  {
+    let question = readlineSync.question(">");
 
-  //   const result = await retrievalChain.invoke({
-  //     input: question
-  //   });
-    
-  //   console.log(result.answer); 
-  //   console.log("==================================================") 
-  // }
+    const result = await retrievalChain.invoke({
+      input: question
+    });
+
+    console.log(result.answer); 
+    console.log("==================================================") 
+  }
 }
 
 main();
